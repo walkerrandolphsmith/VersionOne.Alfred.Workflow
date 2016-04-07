d87358a7c8472199325d45b9bf0f8c9919743bab
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-04-07 12:34:53 -0400

    Initial attempt of invoking a different script (file) from the workflow tray

--- set_single_value_relation.py
+++ set_single_value_relation.py
@@ -5,38 +5,8 @@ from workflow import Workflow, ICON_WEB, ICON_WARNING, web, PasswordNotFound
 log = None
 
 
-def upper_first(x):
-    return x[0].upper() + x[1:]
-
-
-def make_query(url, api_key, params=dict(sel="Name")):
-    headers = dict(Authorization=api_key, Accept="application/json")
-    r = web.get(url, params, headers)
-    r.raise_for_status()
-
-    return r.json()
-
-
-def act_according_to(query, url, api_key):
-    parts = query.split(' ', 3)
-
-    oid, attribute = parts
-    asset_type, asset_number = oid.split(':')
-    # get all the attribute values
-
-    query_url = url + 'rest-1.v1/Data/' + upper_first(asset_type) + upper_first(attribute)
-    assets = make_query(query_url, api_key)['Assets']
-    for asset in assets:
-        asset_oid = asset['id']
-        asset_name = asset['Attributes']['Name']['value']
-        title = asset_name
-        subtitle = 'Set ' + oid + ', ' + upper_first(asset_type) + upper_first(attribute) + ' to ' + asset_name
-        link = oid + " " + asset_oid
-        wf.add_item(title, subtitle, arg=link, valid=True, icon=ICON_WARNING)
-    wf.send_feedback()
-
-
 def main(wf):
+    print('here')
     parser = argparse.ArgumentParser()
     parser.add_argument('--seturl', dest='url', nargs='?', default=None)
     parser.add_argument('--setkey', dest='apikey', nargs='?', default=None)
@@ -66,7 +36,25 @@ def main(wf):
 
     query = args.query
     if query:
-        act_according_to(query, url, api_key)
+        print(query)
+        oid, related_oid = query.split(' ')
+        asset_type, asset_number = oid.split(':')
+        query_url = url + 'rest-1.v1/Data/' + asset_type + '/' + asset_number
+        headers = dict(Authorization=api_key, Accept="application/json")
+        params = dict()
+        data = dict()
+        #params = json({
+        # "id": "Story:1185",
+        # "Attributes": {
+        #   "Status": {
+        #   "name": "Status",
+        #   "value": "StoryStatus:135",
+        #   "act": "set"
+        #  }
+        # }
+        #})
+        web.request(method='POST',url=query_url,params=params,data=data,headers=headers)
+
 
 if __name__ == u"__main__":
     wf = Workflow()
